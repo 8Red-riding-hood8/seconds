d266afb69f3efebeea4e564cf0fd54f0ad73bfe7
8Red-riding-hood8 <denysuikmaria@gmail.com>
2024-05-16 19:18:43 +0300

    Without Math.pow

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,3 @@
 let hours_value = window.prompt("Enter count of hours", 0);
-let seconds_value = Math.pow(60, 2) * hours_value;
+let seconds_value = 60 * 60 * hours_value;
 alert(`In ${hours_value} hour(s) - ${seconds_value} seconds`);
